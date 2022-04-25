2fe69089fd1ef25d4304ba5abfc1f824ba9715ab
Gabriel <gabriel.tendongpk71@gmail.com>
2022-04-25 09:18:09 +0100

    update

--- src/users/dto/resetPasswordDto.ts
+++ src/users/dto/resetPasswordDto.ts
@@ -3,12 +3,18 @@ import { IsEmail, IsNotEmpty, Length } from "class-validator";
 
 export class ResetPasswordDto{
 
-    @IsEmail()
+    @IsEmail({
+        message: "Le format de l'email est invalid"
+    })
     @ApiProperty()
     email: string;
 
-    @IsNotEmpty()
-    @Length(8)
+    @IsNotEmpty({
+        message: "Le mot de passe ne doit pas être vide"
+    })
+    @Length(8, 100, {
+        message: "Le mot de passe doit avoir au moins 8 caractères"
+    })
     @ApiProperty()
     newPassword: string;
 
